7b3f2fae61e7d20c60fd4e29782e2a5dc3232c92
UsuarioDeCreme <leosoares.me2@gmail.com>
2024-12-08 17:26:00 -0300

    add vercel json

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world2",
+		})
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	r.Run(":" + port)
+	print("helloworld")
+}
